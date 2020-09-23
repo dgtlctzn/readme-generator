1f7849aa113fe5cb7621b2d01ad69bd5913d3af8
Joseph Perry <josephperry720@gmail.com>
2020-09-23 15:39:40 -0400

    converted init to async function

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+const inquirer = require("inquirer");
+
+// const promptAsync = util.promisify(inquirer.prompt);
+
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
+async function init() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    console.log(answers);
+  } catch {
+      if (err) throw err;
+  }
 }
 
 // function call to initialize program
